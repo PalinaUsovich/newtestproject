5ad72d5aebc8ae267e6d9708b9c563b804df4bbf
Palina Usovich <palina.usovich1995@gmail.com>
2020-05-04 19:33:29 -0400

    Added new script

---
+++ scripts/newscript.js
@@ -0,0 +1,11 @@
+function calculateStudentGrade (score,totalPossibleSCore) {
+    let grade;
+    
+  
+    let scorePercentage = score * 100/totalPossibleSCore;
+
+    if (scorePercentage >=0 && scorePercentage<=59) {
+        grade = 'F';
+    }
+}
+
